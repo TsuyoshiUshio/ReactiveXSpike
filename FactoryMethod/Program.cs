b8fd56024e8c0ae7d3669c1ac4b7abf303b574e1
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-06-18 00:36:11 +0900

    Defer method

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 
 namespace FactoryMethod
 {
@@ -7,13 +8,30 @@ namespace FactoryMethod
     {
         static void Main(string[] args)
         {
-            var source = Observable.Generate(0, i => i < 10, i => ++i, i => i * i + 1);
+            var source = Observable.Defer<int>(() =>
+            {
+                Console.WriteLine("# Defer method called.");
+                var s = new ReplaySubject<int>();
+                s.OnNext(1);
+                s.OnNext(2);
+                s.OnNext(3);
+                s.OnCompleted();
+                return s.AsObservable();
+
+            });
 
             var subscription = source.Subscribe(
                 i => Console.WriteLine($"OnNext({i})"),
                 ex => Console.WriteLine($"OnError({ex.Message})"),
                 () => Console.WriteLine("Completed()"));
+
+            var subscription2 = source.Subscribe(
+    i => Console.WriteLine($"OnNext({i})"),
+    ex => Console.WriteLine($"OnError({ex.Message})"),
+    () => Console.WriteLine("Completed()"));
+
             subscription.Dispose();
+            subscription2.Dispose();
 
             Console.ReadLine();
         }
