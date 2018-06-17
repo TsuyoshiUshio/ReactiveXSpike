5f46834449597425ecb6ccc7a90383243ccd8880
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-06-18 00:23:26 +0900

    Adding Factory Method sample

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = Observable.Return(10);
+            var subscription = source.Subscribe(
+                i => Console.WriteLine($"OnNext({i})"),
+                ex => Console.WriteLine($"OnError({ex.Message})"),
+                () => Console.WriteLine("Completed()"));
+            subscription.Dispose();
+
+            Console.ReadLine();
+        }
+    }
+}
