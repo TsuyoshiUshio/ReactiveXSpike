0582dcf0131e1c42238bfa9f6d065cc3e55a6972
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-06-17 23:45:18 +0900

    Initial Commit

--- ReactiveSpike/Program.cs
+++ ReactiveSpike/Program.cs
@@ -1,12 +1,113 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ReactiveSpike
 {
+
+    class PrintObserver : IObserver<int>
+    {
+        public void OnNext(int value)
+        {
+            Console.WriteLine($"OnNext({value}) called");
+        }
+        public void OnCompleted()
+        {
+            Console.WriteLine("OnCompleted called.");
+        }
+        public void OnError(Exception error)
+        {
+            Console.WriteLine($"OnError({error.Message}) called.");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var source = new NumberObservable();
+
+            var subscriber1 = source.Subscribe(new PrintObserver());
+            var subscriber2 = source.Subscribe(new PrintObserver());
+
+            Console.WriteLine("## Execute(1) ");
+            source.Execute(1);
+
+            Console.WriteLine("## Dispose");
+            subscriber2.Dispose();
+
+            Console.WriteLine("## Execute(2)");
+            source.Execute(2);
+            Console.WriteLine("## Execute(0)");
+            source.Execute(0);
+
+            var subscriber3 = source.Subscribe(new PrintObserver());
+            Console.WriteLine("## Completed");
+            source.Completed();
+
+            Console.ReadLine();
+        }
+    }
+    
+    class NumberObservable : IObservable<int>
+    {
+        private List<IObserver<int>> observers = new List<IObserver<int>>();
+
+        public void Execute(int value)
+        {
+            if (value == 0)
+            {
+                foreach (var obs in observers)
+                {
+                    obs.OnError(new Exception("value is 0"));
+                }
+                this.observers.Clear();
+                return;
+            }
+            foreach (var obs in observers)
+            {
+                obs.OnNext(value);
+            }
+        }
+
+        public void Completed()
+        {
+            foreach (var obs in observers)
+            {
+                obs.OnCompleted();
+            }
+            this.observers.Clear();
+        }
+
+        public IDisposable Subscribe(IObserver<int> observer)
+        {
+            this.observers.Add(observer);
+            return new RemoveListDisposable(observers, observer);
+        }
+
+        private class RemoveListDisposable : IDisposable
+        {
+            private List<IObserver<int>> observers = new List<IObserver<int>>();
+            private IObserver<int> observer;
+
+            public RemoveListDisposable(List<IObserver<int>> observers, IObserver<int> observer)
+            {
+                this.observers = observers;
+                this.observer = observer;
+            }
+            public void Dispose()
+            {
+                if (this.observers == null)
+                {
+                    return;
+                }
+
+                if (observers.IndexOf(observer) != -1)
+                {
+                    this.observers.Remove(observer);
+                }
+
+                this.observers = null;
+                this.observer = null;
+            }
         }
     }
 }
